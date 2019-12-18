e7c104b689b2a40c4b5d77aa83fd406071226763
khantu12 <khantu12@gmail.com>
2019-12-18 14:15:56 +0100

    creds example in main

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // import * as dereg from './deregister';
 const dereg = require('./deregister');
 
+dereg.creds = {
+    email: "khantu@abv.bg",
+    password: "123456789"
+}
 dereg.get_key().then((key) => console.log(key));
 // dereg.delete_acc();
