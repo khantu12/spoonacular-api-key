bce503cc2c0aa31618fdaca47bca642c7f4298f6
Dimitar Dimitrov <402384@student.fontys.nl>
2019-12-18 15:49:37 +0100

    update

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 const spoonacular = require('./spoonacular');
 
 spoonacular.creds = {
-    email: "khantu@abv.bg",
-    password: "123456789"
-}
+  email: 'khantu@abv.bg',
+  password: '123456789',
+};
 spoonacular.get_key().then((key) => console.log(key));
 // spoonacular.delete_acc();
--- spoonacular.js
+++ spoonacular.js
@@ -1,46 +1,61 @@
 const axios = require('axios');
-const api_regex = /"apiKey":"(.*?)"/i
+const api_regex = /"apiKey":"(.*?)"/i;
 
 const creds = {
-    email: "khantu@abv.bg",
-    password: "123456789"
-}
+  email: 'khantu@abv.bg',
+  password: '123456789',
+};
 
 const get_key = () => {
-    return axios.get('https://spoonacular.com/food-api/console#Dashboard', {withCredentials:true})
+  return axios
+    .get('https://spoonacular.com/food-api/console#Dashboard', {
+      withCredentials: true,
+    })
     .then((response) => {
-        const cookie = response.headers['set-cookie'];
-        return cred(cookie, (logged_cookie) => {
-            return get_api_key(logged_cookie, (key) => key);
-        });
+      const cookie = response.headers['set-cookie'];
+      return cred(cookie, (logged_cookie) => {
+        return get_api_key(logged_cookie, (key) => key);
+      });
     });
-}
+};
 
 const delete_acc = () => {
-    get_key().then((key) => {
-        return axios.get(`https://api.spoonacular.com/spoonacular-api/deleteAccount?apiKey=${key}`);
-    });
-}
+  get_key().then((key) => {
+    return axios.get(
+      `https://api.spoonacular.com/spoonacular-api/deleteAccount?apiKey=${key}`
+    );
+  });
+};
 
 const cred = (cookie, success) => {
-    return axios.get(`https://spoonacular.com/api/logIn?email=${creds.email}&password=${creds.password}`, {headers: {"Cookie":cookie.toString()}})
+  return axios
+    .get(
+      `https://spoonacular.com/api/logIn?email=${creds.email}&password=${creds.password}`,
+      {headers: {Cookie: cookie.toString()}}
+    )
     .then((res) => {
-        if (res.data.status === 'failure') {
-            return axios.get(`https://spoonacular.com/api/register?email=${creds.email}&password=${creds.password}`, {headers: {"Cookie":cookie.toString()}})
-            .then((res => {
-               return cred(cookie, success)
-            }));
-        } else {
-            return success(res.headers['set-cookie']);
-        }
-    })
+      if (res.data.status === 'failure') {
+        return register(cookie).then((res) => cred(cookie, success));
+      }
+      return success(res.headers['set-cookie']);
+    });
+};
+
+const register = (cookie) => {
+  return axios.get(
+    `https://spoonacular.com/api/register?email=${creds.email}&password=${creds.password}`,
+    {headers: {Cookie: cookie.toString()}}
+  );
 };
 
 const get_api_key = (logged_cookie, success) => {
-    return axios.get('https://spoonacular.com/food-api/console', {headers: {"Cookie":logged_cookie.toString()}})
+  return axios
+    .get('https://spoonacular.com/food-api/console', {
+      headers: {Cookie: logged_cookie.toString()},
+    })
     .then((res) => {
-        const api_key = res.data.match(api_regex)[1];
-        return success(api_key);
+      const api_key = res.data.match(api_regex)[1];
+      return success(api_key);
     });
 };
 
