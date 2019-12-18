0b5fdd81a37b82bc90710b852a5f857cbdff9d31
khantu12 <khantu12@gmail.com>
2019-12-18 14:49:48 +0100

    renamed deregister file

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // import * as dereg from './deregister';
-const dereg = require('./deregister');
+const spoonacular = require('./spoonacular');
 
-dereg.creds = {
+spoonacular.creds = {
     email: "khantu@abv.bg",
     password: "123456789"
 }
-dereg.get_key().then((key) => console.log(key));
-// dereg.delete_acc();
+spoonacular.get_key().then((key) => console.log(key));
+// spoonacular.delete_acc();
